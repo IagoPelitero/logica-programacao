3657f37f39d16d1ad953fb0886440b597ce5e314
IagoPelitero <p.iago.ip@gmail.com>
2024-03-27 11:50:31 -0300

    Novo exercício

--- EXERCICIO.md
+++ EXERCICIO.md
@@ -1,3 +1,7 @@
+# Desafio
+
+1. Reescreva os programas "Calculo IMC" e Conversor de Temperatura" para serem executados na própria página HTML
+
 # Exercícios
 
 ## 1. Tipo de um triângulo
@@ -8,7 +12,7 @@ Escreva um programa que leia as medidas dos lados de um triângulo e escreva sua
 * Isósceles: dois lados tem o mesmo tamanho, mas o terceiro é diferente
 * Escaleno: os três lados do triângulo tem tamanhos diferentes
 
-Observação: é preciso verificar se os lados informados são realmente de um rtiângulo, ou seja, se a forma pode ser "fechada". Para isso, a forma devem atender a TODAS as seguinte condição:
+Observação: é preciso verificar se os lados informados são realmente de um rtiângulo, ou seja, se a forma pode ser "fechada". Para isso, a forma devem atender a **TODAS** as seguinte condição:
 
 ~~~math
 ladoA < (ladoB + ladoC) 
--- desafio3.js
+++ desafio3.js
@@ -1,19 +1,16 @@
-function converteTemperatura () {
-
-    const temperatura = parseFloat(window.prompt('Qual valor da temperatura?'));
+function converteTemperatura (valorTemperatura, escalaAtual, escalaConvertida) {
     
-    if (!isNaN(temperatura)) {
-        const escala = window.prompt('Digite a escala atual se é em c ou f ou k').toLowerCase(),
-              conversao = window.prompt('Qual escala deve ser?').toLowerCase(); 
-            if (escala === 'c'){
-                if (conversao === 'f') {
-                    window.alert(`A temperatura ${temperatura} da escala selecionada: ${escala} convertida -> ${((temperatura * 9/5) + 32).toFixed(2)}ºF`);
-                } else if (conversao === 'k') { 
-                    window.alert(`A temperatura ${temperatura} da escala selecionada: ${escala} convertida -> ${(temperatura - 273.15).toFixed(2)}ºK`);
-                } else if (conversao === 'c') { 
-                    window.alert(`Escala selecionada: ${escala}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+    if (!isNaN(valorTemperatura)) {
+
+            if (escalaAtual === 'c'){
+                if (escalaConvertida === 'f') {
+                    window.alert(`A temperatura ${escalaAtual} da escala selecionada: ${escalaConvertida} convertida -> ${((valorTemperatura * 9/5) + 32).toFixed(2)}ºF`);
+                } else if (escalaConvertida === 'k') { 
+                    window.alert(`A temperatura ${escalaAtual} da escala selecionada: ${escalaConvertida} convertida -> ${(valorTemperatura - 273.15).toFixed(2)}ºK`);
+                } else if (escalaConvertida === 'c') { 
+                    window.alert(`Escala selecionada: ${escalaAtual}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
                 } else {
-                    window.alert(`Escala selecionada: ${escala}. Essa escala não existe ou não está disponível neste programa`)
+                    window.alert(`Escala selecionada: ${escalaAtual}. Essa escala não existe ou não está disponível neste programa`)
                 }
             } else if (escala === 'f'){
                 if (conversao === 'k'){
@@ -39,6 +36,6 @@ function converteTemperatura () {
                 window.alert('O número informado não é um número válido')
             }
 
-}
+} return = resultadoConvertido
 
  
