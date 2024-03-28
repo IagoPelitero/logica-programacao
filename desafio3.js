b54473582b78f0d53b60d4de0a7695172b7d662b
Iago Pelitero <160602490+IagoPelitero@users.noreply.github.com>
2024-03-27 23:17:52 -0300

    Código editado para interagir no index
    Atualizado para terminar as demais informações no index

--- desafio3.js
+++ desafio3.js
@@ -4,38 +4,38 @@ function converteTemperatura (valorTemperatura, escalaAtual, escalaConvertida) {
 
             if (escalaAtual === 'c'){
                 if (escalaConvertida === 'f') {
-                    window.alert(`A temperatura ${escalaAtual} da escala selecionada: ${escalaConvertida} convertida -> ${((valorTemperatura * 9/5) + 32).toFixed(2)}ºF`);
+                    resultadoConvertido = (`A temperatura ${valorTemperatura} da escala selecionada: ${escalaAtual} convertida -> ${((valorTemperatura * 9/5) + 32).toFixed(2)}ºF`);
                 } else if (escalaConvertida === 'k') { 
-                    window.alert(`A temperatura ${escalaAtual} da escala selecionada: ${escalaConvertida} convertida -> ${(valorTemperatura - 273.15).toFixed(2)}ºK`);
+                    resultadoConvertido = (`A temperatura ${valorTemperatura} da escala selecionada: ${escalaConvertida} convertida -> ${(valorTemperatura - 273.15).toFixed(2)}ºK`);
                 } else if (escalaConvertida === 'c') { 
-                    window.alert(`Escala selecionada: ${escalaAtual}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+                    resultadoConvertido = (`Escala selecionada: ${escalaAtual}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
                 } else {
-                    window.alert(`Escala selecionada: ${escalaAtual}. Essa escala não existe ou não está disponível neste programa`)
+                    resultadoConvertido = (`Escala selecionada: ${escalaAtual}. Essa escala não existe ou não está disponível neste programa`)
                 }
-            } else if (escala === 'f'){
-                if (conversao === 'k'){
-                    window.alert(`A temperatura ${temperatura} da escala selecionada: ${escala} convertida -> ${((temperatura - 32)* 5/9).toFixed(2)}ºK`);
-                } else if (conversao === `c`) {
-                    window.alert(`A temperatura ${temperatura} da escala selecionada: ${escala} convertida -> ${((temperatura + 459.67) * 5/9).toFixed(2)}ºC`);
-                }  else if (conversao === 'f') { 
-                    window.alert(`Escala selecionada: ${escala}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+            } else if (escalaAtual === 'f'){
+                if (escalaConvertida === 'k'){
+                    resultadoConvertido = (`A temperatura ${valorTemperatura} da escala selecionada: ${escalaAtual} convertida -> ${((valorTemperatura - 32)* 5/9).toFixed(2)}ºK`);
+                } else if (EscalaConvertida === `c`) {
+                    resultadoConvertido = (`A temperatura ${valorTemperatura} da escala selecionada: ${escalaAtual} convertida -> ${((valorTemperatura + 459.67) * 5/9).toFixed(2)}ºC`);
+                }  else if (escalaConvertida === 'f') { 
+                    resultadoConvertido = (`Escala selecionada: ${escalaAtual}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
                 } else {
-                    window.alert(`Escala selecionada: ${escala}. Essa escala não existe ou não está disponível neste programa`)
+                    resultadoConvertido = (`Escala selecionada: ${escala}. Essa escala não existe ou não está disponível neste programa`)
                 }
             } else {
-                if (conversao === 'c') {
-                    window.alert(`A temperatura ${temperatura} da escala selecionada: ${escala} convertida -> ${(temperatura + 273.15).toFixed(2)}ºC`);
-                } else if (conversao === 'f') {
-                    window.alert(`A temperatura ${temperatura} da escala selecionada: ${escala} convertida -> ${((temperatura - 273.15) * 9/5 + 32).toFixed(2)}ºF`);
-                } else if (conversao === 'k') { 
-                    window.alert(`Escala selecionada: ${escala}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
+                if (escalaConvertida === 'c') {
+                    resultadoConvertido = (`A temperatura ${valorTemperatura} da escala selecionada: ${escalaAtual} convertida -> ${(valorTemperatura + 273.15).toFixed(2)}ºC`);
+                } else if (escalaConvertida === 'f') {
+                    resultadoConvertido = (`A temperatura ${valorTemperatura} da escala selecionada: ${escalaAtual} convertida -> ${((valorTemperatura - 273.15) * 9/5 + 32).toFixed(2)}ºF`);
+                } else if (escalaConvertida === 'k') { 
+                    resultadoConvertido = (`Escala selecionada: ${escalaAtual}.Selecione uma escala de CONVERSÃO diferente da ATUAL.`);
                 } else {
-                    window.alert(`Escala selecionada: ${escala}. Essa escala não existe ou não está disponível neste programa`)
+                    resultadoConvertido = (`Escala selecionada: ${escalaAtual}. Essa escala não existe ou não está disponível neste programa`)
                 }
             } } else {
                 window.alert('O número informado não é um número válido')
             }
 
-} return = resultadoConvertido
+} return = resultadoConvertido;
 
  
