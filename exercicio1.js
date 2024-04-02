9335e8d1c5eb47e3ebd984297e944ab0d1d9b5dd
Iago Pelitero <160602490+IagoPelitero@users.noreply.github.com>
2024-04-02 11:24:44 +0000

    Atualizado

--- exercicio1.js
+++ exercicio1.js
@@ -1,23 +1,3 @@
 function ladosTriangulo (ladoA, ladoB, ladoC) {
 
-let resultadoTriangulo;
-
-if ((ladoA + ladoB) > ladoC) {
-
-  resultadoTriangulo = 'isoceles'
-
-} else if ((ladoB + ladoC) > lado A) {
-
-resultadoTriangulo = 'escaleno'
-
-} else if ((ladoA + ladoC) > ladoB) {
-
-resultadoTriangulo = 'escaleno'} else if (ladoA == ladoB && ladoB == ladoC) { 
-
-resultadoTriangulo = 'equilátero' } 
-
-else {resultadoTriangulo = 'não é um triângulo'}
-
-return resultadoTriangulo; 
-
-}
+let resultadoTriangulo;}
